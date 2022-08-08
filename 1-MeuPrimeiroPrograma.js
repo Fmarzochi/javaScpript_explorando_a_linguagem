9182e7258c7fbd25dac918ac9f8f76a1eb83a27c
Felipe Marzochi <97696243+Fmarzochi@users.noreply.github.com>
2022-08-08 17:42:35 -0300

    projetoaluraJS

---
+++ 1-MeuPrimeiroPrograma.js
@@ -0,0 +1,11 @@
+console.log("Meu Primeiro Programa: trabalhando com variávis");
+
+const idade = 37;
+
+console.log("idade");
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
---
+++ 5-AtribuicaoDevariáveis.js
@@ -0,0 +1,22 @@
+console.log("Trabalhando com Atribuiçao Variáveis");
+
+const primeiroNome = "Felipe";
+const sobrenome = "Marzochi";
+
+//console.log(nome + " " + sobrenome);
+console.log(primeiroNome, sobrenome);
+console.log(`Meu Nome é ${primeiroNome} ${sobrenome}`);
+
+let contador = 0;
+contador = contador + 1;
+const nomeCompleto = primeiroNome + sobrenome;
+//nome = 2;
+console.log(nomeCompleto);
+
+let idae; //declarando variável
+idade = 26; //atribuindo valor 
+idade = idade+1;
+console.log(idade);
+
+
+
